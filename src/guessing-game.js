0cfcd7e464fc21743bb383960e50e157ac94b25b
shep-js <kateshakurina@gmail.com>
2023-02-24 11:35:37 +0100

    feat: js quessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.mid = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.mid = Math.ceil((this.max + this.min) / 2);
+    return this.mid;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.mid;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
